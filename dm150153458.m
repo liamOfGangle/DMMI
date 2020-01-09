64d7d7316e876dc02a3ea5fd16391b77e560d6ec
Liam <ljtoverett@gmail.com>
2020-01-09 08:23:11 +0000

    Updated to go through multiple hidden units

--- dm150153458.m
+++ dm150153458.m
@@ -1,72 +1,94 @@
+%% 
+% clear variables/figures and load in data set
+clear all;
+close all;
+load qsar.mat;
 %%
-% clear/close any variables/figures and load in qsar.mat file.
-clear all 
-close all
-load qsar.mat 
-
-z = D(:,11);
-D(:,11) = [];
-X = normalize(D);
+% manipulate data set
+z = D(:,11);  % place column 11 into array z
+D(:,11) = []; % remove col 11 from D
+X = D;        % rename D to X
 %%
-% Choose model building paramaters
+% choose model building parameters
+n = floor(length(X)*0.75);   % choose sample size. 3:1 sample:oos split
+nhid = [1 5 10 15 20 25 30 35 40 45 50]; % # of HU to try
 
-n = floor(length(D)*(3/4)); % choose sample size
-nhid = 10; % number of hidden units
-k = 5; % choose k for k-fold cv
+Broad = 10;
+Fine = 50;
 
-rho = logspace(0,1,200); % grid rho. Intial search is broad
-nits = 200; % number of iterations
+rho = logspace(0,1,Broad); % grid rho. Broad search between 1 and 10
+
+k = 5;      % k for k-fold cv
+nits = 100; % number of iterations
 
 outfunc = 'logistic';
 %%
+% randomly split data into sample:oos
 [x,z,x_star,z_star] = dmrndsplit(X,z,n);
 %%
-PI = zeros(length(rho),1);
-for i=1:length(rho)
-    options = foptions;
-    options(1) = 0;
-    options(14) = nits;
-    mymlp = mlp(size(x,2),nhid,1,outfunc,rho(i));
-    [y_hat,Jemp] = dmxval(mymlp,options,x,z,k);
-    PI(i) = Jemp;
+% run through all # HUs with a broad and then fine (but still a large interval gap) rho grid search. 
+for h=1:length(nhid)
+    PI = zeros(length(rho),1);
+    for i=1:length(rho)
+        options = foptions;
+        options(1) = 0;
+        options(14) = nits;
+        mymlp = mlp(size(x,2),nhid(h),1,outfunc,rho(i));
+        [y_hat,Jemp] = dmxval(mymlp,options,x,z,k);
+        PI(i) = Jemp;
+    end
+    
+    idx = find(PI == min(PI));
+    rho_min = rho(idx);
+    
+    % closest integer values based off the broad minimum search
+    Min = log(floor(rho_min))/log(10);
+    Max = log(ceil(rho_min))/log(10);
+    rho = logspace(Min,Max,Fine); % finer search between Min and Max integers
+    
+    PI = zeros(length(rho),1);
+    for i=1:length(rho)
+        options = foptions;
+        options(1) = 0;
+        options(14) = nits;
+        mymlp = mlp(size(x,2),nhid(h),1,outfunc,rho(i));
+        [y_hat,Jemp] = dmxval(mymlp,options,x,z,k);
+        PI(i) = Jemp;
+    end
+    
+    idx = find(PI == min(PI));
+    
+    C = [PI(idx),rho(idx),nhid(h)];
+    min_check = vertcat(min_check,C);
+    
+    rho_min = rho(idx);
+    
+    % retrain using rho_min;
+    options=foptions; % initialize options
+    options(1)=0; % set "silent"
+    options(14)=nits; % ensure enough iterations allowed
+    mymlp=mlp(size(x,2),nhid(h),1,outfunc,rho_min); % initialize mymlp
+    [mymlp,options]=mlptrain(mymlp,options,x,z); % train mlp
+    y_hat=mlpfwd(mymlp,x); % evaluate on TRAINING sample
+    y_star_hat=mlpfwd(mymlp,x_star); % evaluate oos
+    
+    disp(['value of PI after re-training = ' num2str(options(8))])
+    figure(h);dmroc(z_star,y_star_hat)
+    
+    D = [options(8),nhid(h)];
+    min_retrain = vertcat(min_retrain,D);
 end
 
-figure(1);semilogx(rho,PI);xlabel('ln (\rho)');ylabel('Jemp');
-idx = find(PI == min(PI));
-rho_min = rho(idx); rho_old = rho_min;
-PI_old = PI(idx);
-disp(['best value for rho = ' num2str(rho_min)])
-disp(['minimum value of PI = ' num2str(PI(idx))])
-%%
-Min = log(floor(rho_min))/log(10);
-Max = log(ceil(rho_min))/log(10);
-rho = logspace(Min,Max,200); % finer search for rho based off of the lowest value
-PI = zeros(length(rho),1);
-for j=1:length(rho)
-    options = foptions;
-    options(1) = 0;
-    options(14) = nits;
-    mymlp = mlp(size(x,2),nhid,1,outfunc,rho(j));
-    [y_hat,Jemp] = dmxval(mymlp,options,x,z,k);
-    PI(j) = Jemp;
-end
 
-figure(2);semilogx(rho,PI);xlabel('ln (\rho)');ylabel('Jemp');
-idx = find(PI == min(PI));
-rho_min = rho(idx); rho_new = rho_min;
-PI_new = PI(idx);
-disp(['best value for rho = ' num2str(rho_min)])
-disp(['minimum value of PI = ' num2str(PI(idx))])
-%%
-% retrain using rho_min;
-options=foptions; % initialize options
-options(1)=0; % set "silent"
-options(14)=nits; % ensure enough iterations allowed
-mymlp=mlp(size(x,2),nhid,1,outfunc,rho_min); % initialize mymlp
-[mymlp,options]=mlptrain(mymlp,options,x,z); % train mlp
-y_hat=mlpfwd(mymlp,x); % evaluate on TRAINING sample
-y_star_hat=mlpfwd(mymlp,x_star); % evaluate oos
-%%
-disp(['value of PI after re-training = ' num2str(options(8))])
-figure(3);dmroc(z_star,y_star_hat)
 
+
+
+
+
+
+    
+    
+    
+    
+    
+    
